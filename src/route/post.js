7da8dc39d9296e01a94b463120be65f153512995
jayanth40 <jayanth7996@gmail.com>
2023-02-25 21:12:31 +0530

    added

--- src/route/post.js
+++ src/route/post.js
@@ -21,7 +21,7 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage },{
-  limits: { fileSize: 50 * 1024 * 1024 }
+  limits: { fileSize: 10 * 1024 * 1024 }
 });
 
 // Require your Post model
